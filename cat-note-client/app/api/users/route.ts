e90cda0a85032347bb2307ee83ba0932f412d81b
MaliyOna <veronika.pr777@gmail.com>
2023-12-03 21:53:34 +0300

    add connection with backend

---
+++ cat-note-client/app/api/users/route.ts
@@ -0,0 +1,56 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { NextResponse } from "next/server";
+import { api } from "../api";
+
+const backendApi = process.env.NEXT_PUBLIC_API_URL;
+
+export async function GET(req: any) {
+    const {accessToken} = await getAccessToken(req);
+
+    console.log(accessToken);
+    const response = await fetch(`${backendApi}/api/user`,
+        {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+    const responseJson = await response.json();
+    console.log(responseJson);
+    return Response.json(responseJson);
+
+    //const responseJson = await response.json();
+    //return Response.json(body);
+
+
+    //return new NextResponse(await response.json());
+    //return NextResponse.json(response.json());
+    //console.log(Response.json(response));
+    //return response.body;
+
+}
+
+// export async function getTasks() {
+//     console.log("1");
+//     const  accessToken  = await getAccessToken(request);
+//     console.log(accessToken);
+
+//     //const response = await fetch(`http://localhost:5237/api/task`);
+//     const response = await api.get(`/user`);
+
+// //     , {
+// //     headers: {
+// //         Authorization: `Bearer ${accessToken}`
+// //     }
+// // }
+
+//     //return NextResponse.json(response);
+//     //const responseJson = response.json();
+//     console.log(response);
+//     return NextResponse.json({message: 'hi'});
+//     //return NextResponse.json(response.data);
+// }
+
+// export async function GET(request: Request) {
+//     return NextResponse.json({message: 'hi'});
+// }
--- cat-note-client/app/page.tsx
+++ cat-note-client/app/page.tsx
@@ -1,18 +1,35 @@
 'use client'
-import React from "react";
+import React, {useState} from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { NextResponse } from "next/server";
+//import { getTasks } from "./api/users/route";
 
-function page() {
+function Page() {
   const { user } = useUser();
+  const [myUser, setMyUser] = useState<any>(null);
+
 
   async function getTasksClick() {
     await handleGet()
   }
 
   const handleGet = async () => {
-    await fetch("/api/tasks", { method: "GET" }).then((res) => {
+    console.log("123")
+    //const data = await getTasks();
+
+    await fetch("/api/users", { method: "GET" }).then((res) => {
+      setMyUser(res);
       console.log(res);
     });
+
+    if (myUser) {
+      console.log("4324")
+      const data = await myUser.json();
+
+      if (data) {
+        console.log(data)
+      }
+    }
   };
 
   if (user) {
@@ -20,7 +37,8 @@ function page() {
     return (
       <>
         <div onClick={() => getTasksClick()}>click</div>
-        <div>"Hi"</div>
+        <a href="/api/auth/login">Login</a>
+        <div>Hi</div>
       </>
     )
   }
@@ -28,16 +46,9 @@ function page() {
     console.log("нет");
     return (
       <>
-        <a href="/api/auth/login">Login</a>
       </>
     )
   }
-
-  return (
-    <>
-      <a href="/api/auth/login">Login</a>
-    </>
-  )
 }
 
-export default page;
+export default Page;
