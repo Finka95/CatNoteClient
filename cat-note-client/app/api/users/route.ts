fd0b6c6b1cf91b345078695b44d80ce1b232bee7
Veronika Pristromskaya <veronika.pr777@gmail.com>
2023-12-05 09:42:17 +0100

    get json

--- cat-note-client/app/api/users/route.ts
+++ cat-note-client/app/api/users/route.ts
@@ -1,6 +1,5 @@
 import { getAccessToken } from "@auth0/nextjs-auth0";
 import { NextResponse } from "next/server";
-import { api } from "../api";
 
 const backendApi = process.env.NEXT_PUBLIC_API_URL;
 
@@ -15,41 +14,5 @@ export async function GET(req: any) {
         });
 
     const responseJson = await response.json();
-    console.log(responseJson);
-    return Response.json(responseJson);
-
-    //const responseJson = await response.json();
-    //return Response.json(body);
-
-
-    //return new NextResponse(await response.json());
-    //return NextResponse.json(response.json());
-    //console.log(Response.json(response));
-    //return response.body;
-
+    return NextResponse.json(responseJson);
 }
-
-// export async function getTasks() {
-//     console.log("1");
-//     const  accessToken  = await getAccessToken(request);
-//     console.log(accessToken);
-
-//     //const response = await fetch(`http://localhost:5237/api/task`);
-//     const response = await api.get(`/user`);
-
-// //     , {
-// //     headers: {
-// //         Authorization: `Bearer ${accessToken}`
-// //     }
-// // }
-
-//     //return NextResponse.json(response);
-//     //const responseJson = response.json();
-//     console.log(response);
-//     return NextResponse.json({message: 'hi'});
-//     //return NextResponse.json(response.data);
-// }
-
-// export async function GET(request: Request) {
-//     return NextResponse.json({message: 'hi'});
-// }
--- cat-note-client/app/page.tsx
+++ cat-note-client/app/page.tsx
@@ -1,36 +1,21 @@
 'use client'
 import React, { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { NextResponse } from "next/server";
-//import { getTasks } from "./api/users/route";
 
 function Page() {
   const { user } = useUser();
-  const [myUser, setMyUser] = useState<any>(null);
-
+  const [myUsers, setMyUsers] = useState<any>([]);
 
   async function getTasksClick() {
     await handleGet()
   }
 
   const handleGet = async () => {
-    console.log("123")
-    //const data = await getTasks();
-
-    await fetch("/api/users", { method: "GET" }).then((res) => {
-      setMyUser(res);
-      console.log(res);
-    });
-
-    if (myUser) {
-      console.log("4324");
-      const data = await myUser.json();
-
-      if (data) {
-        console.log(data)
-      }
-      console.log("ошибка")
-    }
+    await fetch("/api/users", { method: "GET" })
+      .then(async (res) => {
+        const usersResponce = await res.json();
+        setMyUsers(usersResponce);
+      });
   };
 
   if (user) {
