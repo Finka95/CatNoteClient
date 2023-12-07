204655c48d163155b4256b7af63d0a447aafb5ff
Veronika Pristromskaya <veronika.pr777@gmail.com>
2023-12-07 13:39:10 +0100

    add header

--- cat-note-client/app/pages/tasks/page.tsx
+++ cat-note-client/app/pages/tasks/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './page.scss';
+import HeaderWithMenu from '@/app/shared/components/headerWithMenu/headerWithMenu';
 
 function TasksPage() {
   return (
     <>
+      <HeaderWithMenu />
       Page for tasks
     </>
   );
