f49c1b6f36387348cc1c5fa3a5498a4f2cefe9ea
Veronika Pristromskaya <veronika.pr777@gmail.com>
2023-12-08 07:51:31 +0100

    add auth0 and getUsers(to check the code)

--- cat-note-client/app/page.tsx
+++ cat-note-client/app/page.tsx
@@ -1,11 +1,40 @@
-import Image from 'next/image'
+'use client'
+
 import HeaderWithMenu from './shared/components/headerWithMenu/headerWithMenu'
+import { useUser } from "@auth0/nextjs-auth0/client";
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <HeaderWithMenu />
-      CatNote here!
-    </>
-  )
+  const { user } = useUser();
+  const [myUsers, setMyUsers] = useState<any>([]);
+
+  async function getUsersClick() {
+    await handleGet()
+  }
+
+  const handleGet = async () => {
+    await fetch("/api/users", { method: "GET" })
+      .then(async (res) => {
+        const usersResponce = await res.json();
+        setMyUsers(usersResponce);
+      });
+  };
+
+  if (user) {
+    return (
+      <>
+        <HeaderWithMenu></HeaderWithMenu>
+        <div onClick={() => getUsersClick()}>click</div>
+        <a href="/api/auth/logout">Logout</a>
+        <div>Welcome to CatNote!</div>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <a href="/api/auth/login">Login</a>
+      </>
+    )
+  }
 }
