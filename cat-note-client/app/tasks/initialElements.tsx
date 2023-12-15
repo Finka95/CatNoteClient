e03d93d76c36f2beeda88aba98a109149a14306c
MaliyOna <veronika.pr777@gmail.com>
2023-12-15 17:06:43 +0300

    add react flow

---
+++ cat-note-client/app/tasks/initialElements.tsx
@@ -0,0 +1,13 @@
+const nodeStyle = {
+    color: '#0041d0',
+    borderColor: '#0041d0',
+};
+
+export default [
+    {
+        type: 'input',
+        id: '1',
+        data: { label: <div style={{ color: 'red' }}>123</div>},
+        position: { x: 150, y: 0 },
+    }
+];
--- cat-note-client/app/tasks/page.tsx
+++ cat-note-client/app/tasks/page.tsx
@@ -1,11 +1,32 @@
+'use client'
+
 import React from 'react';
+import ReactFlow, { Controls, Background, addEdge, useEdgesState, useNodesState } from 'reactflow';
+import InitialNodes from './initialElements';
+import 'reactflow/dist/style.css';
 import './page.scss';
 
 function TasksPage() {
+  const nodeStyle = {
+    color: '#0041d0',
+    borderColor: '#0041d0',
+  };
+
+  const[nodes, setNodes, onNodesChange] = useNodesState(InitialNodes);
+
   return (
-    <>
-      Page for tasks
-    </>
+    <div className='tasksPage'>
+      <ReactFlow
+        className='tasksPage__reactFlow'
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        fitView
+        nodesDraggable
+      >
+        <Background className='tasksPage__background'/>
+        <Controls />
+      </ReactFlow>
+    </div>
   );
 }
 
