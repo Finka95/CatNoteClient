8b4ca7ef84a1ca6d29e9be453498241e32993e16
Veronika Pristromskaya <76497091+MaliyOna@users.noreply.github.com>
2023-12-21 09:18:18 +0100

    Add a react flow and add test element (#8)

--- cat-note-client/app/tasks/page.tsx
+++ cat-note-client/app/tasks/page.tsx
@@ -1,11 +1,26 @@
+'use client'
+
 import React from 'react';
+import ReactFlow, { Controls, Background, useNodesState } from 'reactflow';
+import InitialNodes from './initialElements';
+import 'reactflow/dist/style.css';
 import './page.scss';
 
 function TasksPage() {
+  const[nodes, setNodes, onNodesChange] = useNodesState(InitialNodes);
+
   return (
-    <>
-      Page for tasks
-    </>
+    <div className='tasksPage'>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        fitView
+        nodesDraggable
+      >
+        <Background className='tasksPage__background'/>
+        <Controls />
+      </ReactFlow>
+    </div>
   );
 }
 
