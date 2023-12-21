65f71d6b011230ae724961b1f01f9ceb8eab85c4
Veronika Pristromskaya <76497091+MaliyOna@users.noreply.github.com>
2023-12-21 08:42:24 +0100

    Add a list of achievements to the achievement page with rules for achieve it (#7)

---
+++ cat-note-client/app/components/ui-components/accordion/accordion.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './accordion.scss';
+
+type Props = {
+  title: string | undefined,
+  description: string | undefined
+}
+
+export default function BasicAccordion(props: Props) {
+  return (
+    <div className='basicAccordion'>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{props.title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            {props.description}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
